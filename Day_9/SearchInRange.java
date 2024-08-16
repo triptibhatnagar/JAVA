3983d6ece62282bf6edc6668b39c68a4702c30a8
triptibhatnagar <bhatnagartripti2727@gmail.com>
2024-08-16 17:06:32 +0530

    revise

--- Day_9/SearchInRange.java
+++ Day_9/SearchInRange.java
@@ -3,12 +3,12 @@ package Day_9;
 import java.util.Arrays;
 
 public class SearchInRange {
-    static int search(int arr[], int start, int end, int target) {
+    static int search(int arr[], int start, int end, int item) {
         if(arr.length == 0) {//arr is empty
             return -1;
         }
         for (int i = start; i <= end; i++) {
-            if(arr[i] == target) {
+            if(arr[i] == item) {
                 return i;
             }
         }
@@ -16,11 +16,11 @@ public class SearchInRange {
     }
     public static void main(String[] args) {
         int arr[] = {12, 23, 8, 87, 45, 7, 87, 32, 54, 1, 57, 49};
-        int target = 1;
+        int item = 1;
         int start = 3;
         int end = 8;
 
-        System.out.println(search(arr, start, end, target));
+        System.out.println(search(arr, start, end, item));
         System.out.println(Arrays.toString(arr));
 
         //in array -> length is a variable => arr.length
